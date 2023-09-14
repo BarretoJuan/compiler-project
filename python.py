c731c89102d41117119b52fb1af1b544ef0e7eba
Juan Barreto <juan2.manuelbarreto@gmail.com>
2023-09-14 07:37:16 -0400

    implemented open file functionality: @

--- python.py
+++ python.py
@@ -1,22 +1,45 @@
 from tkinter import * 
 from tkinter.ttk import * 
-from open_file import *
+import tkinter.filedialog
+
+#file path variable declaration
+global file_path 
+file_path = None
+
+#file selector function
+def select_file():
+    #declare permitted filetypes (so far only .txt)
+    global file_path
+    filetypes = (
+        [("Text files", "*.txt")] 
+    )
+
+    #open file selector dialog
+    file_path = tkinter.filedialog.askopenfilename(filetypes=filetypes)
+
+    #text file reading and insertion to the text area
+    if file_path:
+        with open(file_path, 'r') as file:
+            content = file.read()
+    text_area.delete(1.0, END) 
+    text_area.insert(INSERT, content)
+    #update main window title
+    root.title('Codificador EstudioVisual | '+ str(file_path))
   
 #tkinter window
 root = Tk()
+root.title('Codificador EstudioVisual | Sin título' if not file_path else 'Codificador EstudioVisual | '+ str(file_path))
 
-root.title('Codificador EstudioVisual | Sin título')
 
 #setting window icon 
 root.iconbitmap("icon.ico")
 root.state("zoomed")
   
-# Creating Menubar
+#creating Menubar
 menubar = Menu(root)
-
-
   
-# File Menu
+#file Menu
+selected_file = select_file
 file = Menu(menubar, tearoff = 0)
 menubar.add_cascade(label ='Archivo', menu = file)
 file.add_command(label ='Archivo Nuevo', command = None)
@@ -24,16 +47,17 @@ file.add_command(label ='Abrir Archivo', command = select_file)
 file.add_command(label ='Guardar', command = None)
 file.add_command(label ='Guardar Como', command = None)
 
-# Help Menu
+print(select_file)
+#help Menu
 help = Menu(menubar, tearoff = 0)
 menubar.add_cascade(label ='Ayuda', menu = help)
 help.add_command(label ='Acerca de', command = None)
   
-# display Menu
+#display Menu
 root.config(menu = menubar)
 
-# Create a multiline text area (Text widget)
-text_area = Text(root, wrap=WORD, height=1000, width=1000)  # Adjust width and height as needed
-text_area.pack(pady=10, padx=10)  # Adjust pady as needed for spacing
+#multiline text area 
+text_area = Text(root, wrap=WORD, height=1000, width=1000) 
+text_area.pack(pady=10, padx=10) 
 
-mainloop()
+root.mainloop()
