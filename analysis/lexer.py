a60c11cd4a6d73b33ab39ca7cba34843d3ba4d64
Alejandro Lopez <alejandrolpz0619@gmail.com>
2023-11-18 17:46:21 -0400

    finding unrecognizable tokens

--- analysis/lexer.py
+++ analysis/lexer.py
@@ -87,30 +87,58 @@ class TokenType(Enum):
     IDENTIFICATOR = Id
     SYMBOL = Symbol
     NUMBER = Number
-    OPERATION = Op
-    STR_LIT = StringLiteral
+    STRING = StringLiteral
+    OP = Op
+
+class TokenType(Enum):
+    KEYWORD = Keyword
+    IDENTIFICATOR = Id
+    SYMBOL = Symbol
+    NUMBER = Number
+    STRING = StringLiteral
+    OP = Op
+
     @classmethod
-    def parse(cls, text):
+    def parse(cls, text, last_keyword=None, unknown_tokens=[]):
         while text:
             for token_type in cls:
+                if token_type is cls.IDENTIFICATOR:
+                    if last_keyword not in {"function", "var"}:
+                        continue
+                    else:
+                        last_keyword = None  # Reset last_keyword after parsing an identifier
                 token, remainder = token_type.value.parse(text)
                 if token is not None:
-                    return token, remainder
-            text = text[1:]
-        return None, text
+                    if token_type is cls.KEYWORD:
+                        last_keyword = token.value  # Update last_keyword here
+                    return token, remainder, last_keyword, unknown_tokens
+            # Find the next whitespace or special character
+            end_of_token = len(text)
+            for i, char in enumerate(text):
+                if char.isspace(): # finding the next whitespace and slicing the text up to that point.
+                    end_of_token = i
+                    break
+            unknown_tokens.append(text[:end_of_token])
+            text = text[end_of_token:].lstrip()  # Remove leading whitespace
+        return None, text, last_keyword, unknown_tokens
 
-# Then in your main code:
-with open('filename.bre', 'r') as file:
-    content = file.read()
-tokens = []
-for t in content.splitlines():
-    print('THIS LINE IS: ', t)
-    
-    remainder = t
-    while remainder:
-        token, remainder = TokenType.parse(remainder)
-        if token is not None:
-            tokens.append(token)
-        else:
-            break
-print(tokens)
+def parse(filepath):
+    # Open it in read mode
+    with open(filepath, 'r') as file:
+        content = file.read()
+    tokens = [] # We will store each token in this list
+    unknown_tokens = [] # We will store unknown tokens in this list
+    last_keyword = None # We will store the last keyword in this variable to keep track of it
+    for l in content.splitlines():
+        remainder = l
+        while remainder:
+            token, remainder, last_keyword, unknown_tokens = TokenType.parse(remainder, last_keyword, unknown_tokens)
+            if token is not None:
+                tokens.append(token)
+            else:
+                break
+    unknown_tokens = [token for token in unknown_tokens if token != '']
+    print("Tokens:", tokens)
+    print("Unrecognizable tokens:", unknown_tokens)
+
+parse('filename.bre')
